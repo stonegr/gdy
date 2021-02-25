47e3c7e78222fe396e257ca637099d99208f4348
stonegr <74632994+stonegr@users.noreply.github.com>
2021-02-25 08:29:02 +0800

    Update diy-part2-2.sh

--- diy-part2-2.sh
+++ diy-part2-2.sh
@@ -14,5 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i '11s/4.19/5.4/g' target/linux/ipq40xx/Makefile
-sed -i 's/root::0:0:99999:7:::/root:$1$ZzLkZmEb$Kpkpyxaj6bYCEtrs7LqWs.:18612:0:99999:7:::/g' package/base-files/files/etc/shadow
-rm -rf feeds/packages/net/smartdns
+
